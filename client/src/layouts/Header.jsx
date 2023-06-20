0f3fdde851acdf93e931b6e34c5a281f3cbeae93
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-06-20 17:34:47 +0900

    style: 아이콘 적용

--- client/src/layouts/Header.jsx
+++ client/src/layouts/Header.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 import styled from "styled-components";
+import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
 
 const HeaderWrapper = styled.div`
   height: 50px;
@@ -8,15 +11,35 @@ const HeaderWrapper = styled.div`
   background-color: var(--third-theme-color);
 `;
 
-const LogoStyle = styled.div``;
+const LogoStyle = styled.div`
+  margin-left: 20px;
+`;
 
-const NavListIcon = styled.div``;
+const NavListIcon = styled.div`
+  margin-right: 20px;
+  color: var(--first-theme-color);
+  cursor: pointer;
+`;
+
+const CustomHamburgerMenu = styled(RxHamburgerMenu)`
+  width: 30px;
+  height: 30px;
+`;
+
+const CustomCross1 = styled(RxCross1)`
+  width: 30px;
+  height: 30px;
+`;
 
 export default function Header() {
+  const [NavList, setNavList] = useState(false);
+
   return (
     <HeaderWrapper>
       <LogoStyle>여기 여기 붙어라</LogoStyle>
-      <NavListIcon>네브리스트</NavListIcon>
+      <NavListIcon onClick={() => setNavList(!NavList)}>
+        {NavList ? <CustomCross1 /> : <CustomHamburgerMenu />}
+      </NavListIcon>
     </HeaderWrapper>
   );
 }
