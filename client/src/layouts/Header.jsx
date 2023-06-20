f7b75558b01974e00b228fb5cc161adb06674533
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-06-20 16:41:39 +0900

    style: 헤더 정렬

--- client/src/layouts/Header.jsx
+++ client/src/layouts/Header.jsx
@@ -1,3 +1,22 @@
+import styled from "styled-components";
+
+const HeaderWrapper = styled.div`
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: var(--third-theme-color);
+`;
+
+const LogoStyle = styled.div``;
+
+const NavListIcon = styled.div``;
+
 export default function Header() {
-  return <div>헤더입니다.</div>;
+  return (
+    <HeaderWrapper>
+      <LogoStyle>여기 여기 붙어라</LogoStyle>
+      <NavListIcon>네브리스트</NavListIcon>
+    </HeaderWrapper>
+  );
 }
