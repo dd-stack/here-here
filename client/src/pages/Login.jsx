b2fc74269fe00c72e867daef08432c814b83192f
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-06-28 22:04:31 +0900

    style: 로그인 페이지 초안 완성

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,3 +1,59 @@
+import styled from "styled-components";
+import { FcConferenceCall } from "react-icons/fc";
+
+import kakaoLogin from "../img/kakao-login.png";
+
+const EntireContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  padding: 70px 20px;
+  background-color: var(--background-color);
+`;
+
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 280px;
+  height: 360px;
+  border-radius: 20px;
+  background-color: #fff;
+  box-shadow: 0 1px 2px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05),
+    0 2px 8px hsla(0, 0%, 0%, 0.05);
+  > img {
+    cursor: pointer;
+  }
+`;
+
+const TextStyle = styled.div`
+  margin: 10px 0;
+  font-size: 18px;
+`;
+
+const LogoStyle = styled.div`
+  margin-bottom: 50px;
+  font-family: "KyoboHand";
+  font-size: 35px;
+  font-weight: 600;
+  text-align: center;
+`;
+
 export default function Login() {
-  return <div>소개 페이지 입니다.</div>;
+  return (
+    <EntireContainer>
+      <LoginContainer>
+        <FcConferenceCall size="50px" />
+        <TextStyle>캐주얼 초대장 서비스</TextStyle>
+        <LogoStyle>
+          여기 여기
+          <br />
+          붙어라
+        </LogoStyle>
+        <img src={kakaoLogin} alt="kakao login button" />
+      </LoginContainer>
+    </EntireContainer>
+  );
 }
