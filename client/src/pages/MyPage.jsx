e6a8385e58d4bd46d977e45997968e807e991c2f
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-06-29 20:01:24 +0900

    feat: 로그인이 필요한 화면 처리

--- client/src/pages/MyPage.jsx
+++ client/src/pages/MyPage.jsx
@@ -1,3 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
+// redux 관련
+import { useSelector } from "react-redux";
+
 export default function MyPage() {
+  const navigate = useNavigate();
+
+  let userInfo = useSelector((state) => state.userInfo);
+
+  // 로그인이 되어 있지 않다면 로그인 화면으로
+  if (userInfo) {
+    navigate("/login");
+  }
+
   return <div>마이 페이지 입니다.</div>;
 }
