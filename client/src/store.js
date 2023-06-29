8a9da992397fffbf600ace93f3390cab23191367
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-06-29 15:56:02 +0900

    feat: 전역 상태 설정

---
+++ client/src/store.js
@@ -0,0 +1,35 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+// 유저 정보 -> 객체 형태
+let userInfo = createSlice({
+  name: "userInfo",
+  initialState: null,
+  reducers: {
+    setUserInfo(state, action) {
+      state.userInfo = action.payload;
+    },
+  },
+});
+
+// 엑세스 토큰 ->
+let accessToken = createSlice({
+  name: "accessToken",
+  initialState: null,
+  reducers: {
+    setAccessToken(state, action) {
+      state.accessToken = action.payload;
+    },
+  },
+});
+
+// 상태 내보내기
+export default configureStore({
+  reducer: {
+    userInfo: userInfo.reducer,
+    accessToken: accessToken.reducer,
+  },
+});
+
+// 변경 함수 내보내기
+export let { setUserInfo } = userInfo.actions;
+export let { setAccessToken } = accessToken.actions;
