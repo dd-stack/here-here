30e74f3225026a2683190ee7418cb9f26ab034e8
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-07-03 04:23:18 +0900

    feat: 유틸 함수 구조 변경

--- client/src/utils/setDataWithExpiry.js
+++ client/src/utils/setDataWithExpiry.js
@@ -1,17 +1,6 @@
-// 데이터를 로컬 스토리지에 저장하고, 유효 시간이 지나면 삭제하는 함수
+// 데이터를 로컬 스토리지에 저장하고, 유효 시간이 지나면 삭제하는 함수 (시간 단위)
 export default function setDataWithExpiry(key, data, expiryTimeInHours) {
-  const now = new Date();
-  // 현재 시간에 유효 시간을 더한 시간
-  const expiryTime = now.getTime() + expiryTimeInHours * 60 * 60 * 1000;
-
-  const item = {
-    data: data,
-    expiry: expiryTime,
-  };
-
-  localStorage.setItem(key, JSON.stringify(item));
-
-  // 지정된 시간 후에 데이터를 삭제
+  localStorage.setItem(key, JSON.stringify(data));
   setTimeout(() => {
     localStorage.removeItem(key);
     // 페이지 새로고침
