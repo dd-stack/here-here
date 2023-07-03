58bdf953caeac15bf7d3345ab874716d8fb30682
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-07-03 12:26:55 +0900

    fix: 페이지 접근 예외 처리 코드 수정

--- client/src/pages/MyPage.jsx
+++ client/src/pages/MyPage.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 // redux 관련
@@ -9,9 +10,11 @@ export default function MyPage() {
   const isLogin = useSelector((state) => state.user?.userInfo);
 
   // 로그인이 되어 있지 않다면 로그인 화면으로
-  if (!isLogin) {
-    navigate("/login");
-  }
+  useEffect(() => {
+    if (!isLogin) {
+      navigate("/login");
+    }
+  }, [isLogin, navigate]);
 
   return <div>마이 페이지 입니다.</div>;
 }
