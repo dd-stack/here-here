c792b3ea3dccc35fbcc908de2a89b0e1ab279edf
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-07-09 15:14:32 +0900

    주석 정리

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,9 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-// 함수 (유효시간 2시간 설정)
 import setDataWithExpiry from "./utils/setDataWithExpiry";
 
-// 엑세스 토큰 + 유저 정보
+// 엑세스 토큰(유효시간 2시간) + 유저 정보
+// refactor point: 유효 시간(만료 시간)을 토큰 디코딩으로 가져오면 더 좋을 듯
 // 새로고침해도 사라지지 않음. -> 이 로직이 계속 필요할 것 같으면,
 // redux-persist 라는 라이브러리도 있음.
 // refactor point: 리프레시 토큰도 사용해 보기
@@ -33,12 +33,10 @@ const userSlice = createSlice({
   },
 });
 
-// 상태 내보내기
 export default configureStore({
   reducer: {
     userSlice: userSlice.reducer,
   },
 });
 
-// 상태 변경 함수 내보내기
 export const { setUserInfo, clearUserInfo, setToken, clearToken } = userSlice.actions;
