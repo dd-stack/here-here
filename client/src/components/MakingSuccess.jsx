c62a58e65f07381b13f7227e40aee743b1729753
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-07-22 06:00:39 +0900

    feat: 공유 페이지 완성, react-share 라이브러리 설치

--- client/src/components/MakingSuccess.jsx
+++ client/src/components/MakingSuccess.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { FacebookShareButton, FacebookIcon, TwitterIcon, TwitterShareButton } from "react-share";
 
 import styled from "styled-components";
 import { FaLink } from "react-icons/fa";
@@ -18,7 +19,7 @@ const ShareUrlContainer = styled.div`
   justify-content: center;
   gap: 10px;
   width: 80%;
-  margin: 40px 0;
+  margin: 10% 0;
   padding: 5%;
   border: 2px solid var(--third-theme-color);
   border-radius: 10px;
@@ -37,14 +38,79 @@ const CopyButton = styled.button`
 `;
 
 const ShareSnsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 25px;
   width: 80%;
-  height: 100px;
-  border: 1px solid var(--gray-color);
+  height: 150px;
+`;
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const KakaoButtonContainer = styled.div`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  overflow: hidden;
 `;
 
 export default function MakingSuccess({ cardId }) {
+  // 실제 도메인 주소로 변경해야 함
   const url = `http://localhost:3000/card/${cardId}`;
   const [isCopied, setIsCopied] = useState(false);
+
+  useEffect(() => {
+    // 최신 Kakao SDK 로드
+    const kakaoScript = document.createElement("script");
+    kakaoScript.src = "https://t1.kakaocdn.net/kakao_js_sdk/2.3.0/kakao.min.js";
+    kakaoScript.async = true;
+    kakaoScript.integrity =
+      "sha384-70k0rrouSYPWJt7q9rSTKpiTfX6USlMYjZUtr1Du+9o4cGvhPAWxngdtVZDdErlh";
+    kakaoScript.crossOrigin = "anonymous";
+    document.body.appendChild(kakaoScript);
+
+    kakaoScript.onload = () => {
+      if (window.Kakao) {
+        const Kakao = window.Kakao;
+        // 초기화되지 않았다면 초기화
+        if (!Kakao.isInitialized()) {
+          Kakao.init(process.env.REACT_APP_JAVASCRIPT_KEY);
+        }
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    // 카카오 링크 버튼 생성
+    if (window.Kakao && document.querySelector("#kakaotalk-sharing-btn")) {
+      const Kakao = window.Kakao;
+      Kakao.Share.createDefaultButton({
+        container: "#kakaotalk-sharing-btn",
+        objectType: "feed",
+        content: {
+          title: "띵동! 초대장이 도착했어요 ~🎵",
+          description: "자세히 보기를 눌러 확인해보세요!",
+          imageUrl:
+            "https://github.com/dd-stack/here-here/blob/fe/client/src/img/letter.png?raw=true",
+          link: {
+            webUrl: url,
+          },
+        },
+        buttons: [
+          {
+            title: "자세히 보기",
+            link: {
+              webUrl: url,
+            },
+          },
+        ],
+      });
+    }
+  }, []);
 
   const copyToClipboard = () => {
     navigator.clipboard.writeText(url);
@@ -63,7 +129,25 @@ export default function MakingSuccess({ cardId }) {
           {isCopied ? "복사 완료!" : "URL 복사하기"}
         </CopyButton>
       </ShareUrlContainer>
-      <ShareSnsContainer>카카오 인스타 트위터</ShareSnsContainer>
+      <ShareSnsContainer>
+        <span>SNS로 공유하기</span>
+        <ButtonsWrapper>
+          <KakaoButtonContainer>
+            <a id="kakaotalk-sharing-btn" href="javascript:;">
+              <img
+                src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png"
+                alt="카카오톡 공유 보내기 버튼"
+              />
+            </a>
+          </KakaoButtonContainer>
+          <FacebookShareButton url={url}>
+            <FacebookIcon size={50} round={true}></FacebookIcon>
+          </FacebookShareButton>
+          <TwitterShareButton url={url}>
+            <TwitterIcon size={50} round={true}></TwitterIcon>
+          </TwitterShareButton>
+        </ButtonsWrapper>
+      </ShareSnsContainer>
     </EntireContainer>
   );
 }
