fa18c271af4b0bb1a247c0aa2e94d1fb0cfce04c
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-07-22 06:10:03 +0900

    feat: 카드 페이지 라우터 설정, 예외 처리, 배포 시 console.log 지우기

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 // 라우터 세팅
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 // import layouts
 import Header from "./layouts/Header";
@@ -10,11 +10,17 @@ import Login from "./pages/Login";
 import Redirection from "./pages/Redirection";
 import MyPage from "./pages/MyPage";
 import Making from "./pages/Making";
+import Card from "./pages/Card";
 
 // import etc
 import GlobalStyles from "./styles/GlobalStyles";
 
 export default function App() {
+  // 배포 환경에서 console.log 지우기
+  if (process.env.NODE_ENV === "production") {
+    console.log = function no_console() {};
+  }
+
   return (
     <div className="App">
       <GlobalStyles />
@@ -26,6 +32,8 @@ export default function App() {
           <Route path="/login/oauth2/code/kakao" element={<Redirection />} />
           <Route path="/mypage" element={<MyPage />} />
           <Route path="/making" element={<Making />} />
+          <Route path="/card/:id" element={<Card />} />
+          <Route path="/*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
     </div>
