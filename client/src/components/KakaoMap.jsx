cacba8e68da4b0e7df273727c9fab0afea0b44d8
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-07-22 17:17:44 +0900

    feat: 변수 설정, 주석 정리

--- client/src/components/KakaoMap.jsx
+++ client/src/components/KakaoMap.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 
-export default function KakaoMap() {
+export default function KakaoMap({ location }) {
   const mapContainerRef = useRef(null);
 
   useEffect(() => {
@@ -9,29 +9,30 @@ export default function KakaoMap() {
       level: 3,
     };
 
-    // 지도를 생성합니다
+    // 지도 생성
     const map = new window.kakao.maps.Map(mapContainerRef.current, mapOptions);
 
-    // 주소-좌표 변환 객체를 생성합니다
+    // 주소 - 좌표 변환 객체 생성
     const geocoder = new window.kakao.maps.services.Geocoder();
 
-    // 주소로 좌표를 검색합니다
-    geocoder.addressSearch("제주특별자치도 제주시 첨단로 242", function (result, status) {
-      // 정상적으로 검색이 완료됐으면
+    // 주소로 좌표 검색
+    geocoder.addressSearch(`${location}`, function (result, status) {
+      // 정상적으로 검색이 완료됐으면,
       if (status === window.kakao.maps.services.Status.OK) {
         const coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
 
-        // 결과값으로 받은 위치를 마커로 표시합니다
+        // 결과값으로 받은 위치를 마커로 표시한다.
         const marker = new window.kakao.maps.Marker({
           map: map,
           position: coords,
         });
 
-        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+        // 지도 중심 이동 (결과값으로 받은 위치로)
         map.setCenter(coords);
       }
     });
   }, []);
+
   return (
     <div>
       <div id="map" style={{ width: "500px", height: "400px" }} ref={mapContainerRef} />
