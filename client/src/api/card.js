2a014788526d93f8199cf0e988700f4ef45d5084
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-07-22 17:15:12 +0900

    feat: getCard api 추가

--- client/src/api/card.js
+++ client/src/api/card.js
@@ -1,8 +1,19 @@
+import axios from "./core/instance";
 import { authAxios } from "./core/instance";
 
 export const postCard = async (card) => {
   try {
     const response = await authAxios.post("/card", card);
+    return response;
+  } catch (error) {
+    console.log(error);
+    return "fail";
+  }
+};
+
+export const getCard = async () => {
+  try {
+    const response = await axios.get("/card");
     return response;
   } catch (error) {
     console.log(error);
