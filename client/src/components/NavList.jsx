c695886ff0af45b11b4a7e69c94da770106e397c
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-08-03 01:26:12 +0900

    fix: 로그아웃 로직 수정

--- client/src/components/NavList.jsx
+++ client/src/components/NavList.jsx
@@ -39,15 +39,19 @@ export default function NavList({ isOpen, setIsOpen, onMakingNavClick, onMypageN
 
   // 로그아웃
   const handleClick = async () => {
-    await logout();
-    // 엑세스 토큰 & 리프레시 토큰 & 유저 정보 & 페이지 정보 삭제
-    sessionStorage.removeItem("accessToken");
-    sessionStorage.removeItem("refreshToken");
-    dispatch(clearUserInfo());
+    // 로그아웃 요청 (카카오 토큰 삭제)
+    await logout().then(() => {
+      // 요청이 성공이어도 실패여도 프론트 단은 로그아웃 처리
+      // 엑세스 토큰 & 리프레시 토큰 & 유저 정보 삭제
+      sessionStorage.removeItem("accessToken");
+      sessionStorage.removeItem("refreshToken");
+      dispatch(clearUserInfo());
+      // 홈 화면으로 라우팅, NavList 닫기
+      navigate("/");
+      setIsOpen(false);
+    });
+    // 페이지 정보 삭제
     sessionStorage.removeItem("cardId");
-    // 홈 화면으로 라우팅, NavList 닫기
-    navigate("/");
-    setIsOpen(false);
   };
 
   return (
