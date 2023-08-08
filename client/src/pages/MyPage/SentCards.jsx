f5b5b847c0669f9040f2d37cdf5046d38930527b
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-08-09 02:19:08 +0900

    fix: confirm 취소 눌렀을 때도 삭제 요청 가는 문제 해결

--- client/src/pages/MyPage/SentCards.jsx
+++ client/src/pages/MyPage/SentCards.jsx
@@ -50,25 +50,27 @@ export default function SentCards() {
       confirmButtonText: "예",
       cancelButtonText: "아니오",
       padding: "20px 40px 40px",
-    }).then(() => {
-      // 삭제 요청
-      deleteCard(id).then((result) => {
-        if (result === "success") {
-          Swal.fire({
-            text: "초대장이 삭제되었습니다.",
-            icon: "success",
-            confirmButtonColor: "var(--link-color)",
-            confirmButtonText: "확인",
-            padding: "20px 40px 40px",
-          }).then(() => {
-            // 삭제 후 새로고침
-            window.location.reload();
-          });
-        }
-        if (result === "fail") {
-          alert("초대장 삭제에 실패했습니다. 다시 시도해 주시기 바랍니다.");
-        }
-      });
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // 삭제 요청
+        deleteCard(id).then((result) => {
+          if (result === "success") {
+            Swal.fire({
+              text: "초대장이 삭제되었습니다.",
+              icon: "success",
+              confirmButtonColor: "var(--link-color)",
+              confirmButtonText: "확인",
+              padding: "20px 40px 40px",
+            }).then(() => {
+              // 삭제 후 새로고침
+              window.location.reload();
+            });
+          }
+          if (result === "fail") {
+            alert("초대장 삭제에 실패했습니다. 다시 시도해 주시기 바랍니다.");
+          }
+        });
+      }
     });
   };
 
